2ae8f1166c4fe1ad9462eb1a7cdb2efa51b4d9e0
Arvydas Ramanauskas <arvydas.ramanauskas@evilwoods.net>
2020-09-05 20:54:33 +0200

    adding file to repo

---
+++ DecodingAmericanCivilWarCiphers/storing_key_as_dict.py
@@ -0,0 +1,46 @@
+key = {}
+
+
+def main():
+    print("Enter route cipher key. Press Enter"
+          "r without answer to quit.")
+    while True:
+        column = input("Column number: ")
+        if not column:
+            break
+        direction = input("Direction (up/down): ")
+        if not direction:
+            break
+
+        input_is_valid = validate_input(column, direction)
+        if input_is_valid:
+            column_int, direction_int = convert_input_to_int(column, direction)
+            key[column_int] =  direction_int
+        else:
+            print("Invalid input!")
+
+    print(f"key: {key}")
+
+
+def validate_input(column, direction):
+    valid_directions = {'up', 'down'}
+    if column.isnumeric() and direction in valid_directions:
+        is_valid = True
+    else:
+        is_valid = False
+
+    return is_valid
+
+
+def convert_input_to_int(column, direction):
+    column_int = int(column)
+    if direction == "up":
+        direction_int = 1
+    elif direction == 'down':
+        direction_int = -1
+
+    return column_int, direction_int
+
+
+if __name__ == '__main__':
+    main()
