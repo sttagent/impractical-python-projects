db47943f5ffb97f0927690e46dd3b9d104066f56
Arvydas Ramanauskas <arvydas.ramanauskas@evilwoods.net>
2020-09-07 19:46:43 +0200

    generating a list of column numbers

--- DecodingAmericanCivilWarCiphers/automating_possible_keys.py
+++ DecodingAmericanCivilWarCiphers/automating_possible_keys.py
@@ -3,7 +3,8 @@ from itertools import permutations, product
 
 def generate_possible_keys(number_of_columns):
     results = []
-    for perm in permutations(number_of_columns):
-        for signs in product([-1, 1], repeat=len(number_of_columns)):
+    columns = [number for number in range(1, number_of_columns + 1)]
+    for perm in permutations(columns):
+        for signs in product([-1, 1], repeat=len(columns)):
             results.append([i*sign for i, sign in zip(perm, signs)])
     return results
